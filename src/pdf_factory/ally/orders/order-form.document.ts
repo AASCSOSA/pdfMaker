43049e1a46672db6adad48c52313e974918db02b
alex2 <alex2227@hotmail.com>
2025-03-17 13:31:36 -0600

    feature: Enhanced order form document with new logo and detailed tables for order and account information

--- src/pdf_factory/ally/orders/order-form.document.ts
+++ src/pdf_factory/ally/orders/order-form.document.ts
@@ -13,7 +13,7 @@ export class OrderFormDocument extends DocumentTemplate {
 
   private buildDocument(): void {
     this.addComponent({
-      render: (): Content => ({
+      render: (): Content => ([{
           canvas: [
             {
               type: 'rect',
@@ -21,11 +21,63 @@ export class OrderFormDocument extends DocumentTemplate {
               y: -40,
               w: 595,
               h: 245,
-              r: 0,
-              color: '#13277A'
-            }
-          ]
-        }
+              color: '#026EFA',
+            },
+          ],
+        },
+          {
+            image: 'assets/img/logo-ally.png',
+            absolutePosition: {x: 30, y: 20},
+            width: 134,
+            height: 68.29,
+          },
+          {
+            table: {
+              widths: [70, 290, 105, 75],
+              body: [
+                [
+                  'Razón social',
+                  'CHASE FARMACEUTICAL GROUP S.A DE C.V',
+                  'Estatus de Pedido',
+                  {text: 'Orden', bold: true},
+                ],
+                ['RFC', 'CFG181003SI1', 'Fecha de cotización', '17-03-2023'],
+                ['', '', 'Folio de cotización', '17032023P12'],
+                ['', '', 'Válido hasta', '24-03-2023'],
+              ],
+            },
+            layout: 'noBorders',
+            absolutePosition: {x: 30, y: 110},
+            style: {fontSize: 10, color: '#FFFFFF'},
+          },
+          {
+            table: {
+              widths: [45, 130, 40, '*'],
+              body: [
+                [
+                  {
+                    image: 'assets/img/logo-BBVA.png',
+                    width: 62,
+                    height: 16,
+                    margin: [0, 0, 0, 5],
+                  },
+                  '',
+                  {
+                    image: 'assets/img/logo-santander.png',
+                    width: 83,
+                    height: 16,
+                    margin: [0, 0, 0, 5],
+                  },
+                  '',
+                ],
+                ['Cuenta', '65-50720658-9', 'Cuenta', '0117772076'],
+                ['CLABE', '014580655072065895', 'CLABE', '012580001177720762'],
+              ],
+            },
+            layout: 'noBorders',
+            absolutePosition: {x: 30, y: 170},
+            style: {fontSize: 10, color: '#FFFFFF', marginTop: 20},
+          },]
       )
     });
     this.addComponent({
@@ -58,8 +110,8 @@ export class OrderFormDocument extends DocumentTemplate {
     ];
     this.addComponent(new SectionComponent('Desglose de Precio')
       .setWidth(515)
-      .setAxisTextPosition(10, -40)
-      .setTextColor('#ff0000')
+      .setAxisTextPosition(10, -20)
+      .setTextColor('#ffffff')
       .setSectionColor('#13277A')
     );
     this.addComponent(new TableComponent()
@@ -152,6 +204,19 @@ export class OrderFormDocument extends DocumentTemplate {
       },
       h5: {
         fontSize: 10,
+      },
+      headerCell: {
+        bold: true,
+        fontSize: 10,
+        fillColor: '#E6F7F7',
+        color: '#111118',
+      },
+
+      sectionHeader: {
+        fontSize: 12,
+        color: '#ffffff',
+        alignment: 'center',
+        bold: true,
       },
     };
   }
