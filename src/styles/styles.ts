4be58cc9b30173c08095d2b6381ce9372d5ac3f4
ISC-Lorenz <isc.lorenzovr@gmial.com>
2025-03-19 14:20:13 -0600

    update from master

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -4,7 +4,8 @@ export enum Colors {
   WHITE = '#FFFFFF',
   TOREA_BAY = '#0C2788',
   FOAM = '#DDFCF9',
-  BLUE_RIBBON = '#006EFF',
+  DENIM = '#006EFF',
+  BLUE_RIBBON = '#026EFA',
   HAWKES_BLUE = '#EBF2FE',
   MIDNIGHT_BLUE = '#00366A',
   BLACK = '#111118',
@@ -17,33 +18,25 @@ export enum Alignments {
   CENTER = 'center',
 }
 
-/*function getSvgAsBase64(filePath: string): string {
-  const svg = fs.readFileSync(filePath, 'utf8');
-  const base64Data = Buffer.from(svg).toString('base64');
-  return `data:image/svg+xml;base64,${base64Data}`;
-}*/
-
 export enum LogoKeys {
   ALLY = 'ALLY',
   FGO = 'FGO',
   GO = 'GO',
+  LOGOBBVA = 'LOGOBBVA',
+  LOGOSANTANDER = 'LOGOSANTANDER',
+  FARMA_GO = 'FARMA_GO',
 }
 
 export const Logos: Record<LogoKeys, string> = {
   [LogoKeys.ALLY]: 'assets/img/logo-ally.png',
   [LogoKeys.FGO]: 'assets/img/logo-fgo.png',
   [LogoKeys.GO]: 'assets/img/go.png',
+  [LogoKeys.LOGOBBVA]: 'assets/img/logo-BBVA.png',
+  [LogoKeys.LOGOSANTANDER]: 'assets/img/logo-santander.png',
+  [LogoKeys.FARMA_GO]: 'assets/img/FarmaGO.png',
 };
 
 export enum Fonts {
-  InterNormal = 'normal',
-  InterBold = 'bold',
-  InterItalics = 'italics',
-  InterBoldItalics = 'bolditalics',
-  RobotoNormal = 'robotoNormal',
-  RobotoBold = 'robotoBold',
-  RobotoBoldItalics = 'robotoBoldItalics',
-  RobotoItalics = 'robotoItalics',
   Inter_400 = 'Inter_400',
   Inter_500 = 'Inter_500',
   Inter_600 = 'Inter_600',
@@ -52,9 +45,32 @@ export enum Fonts {
   Roboto_600 = 'Roboto_600',
   Roboto_500 = 'Roboto_500',
   Roboto_400 = 'Roboto_400',
+  Roboto_900 = 'Roboto_900',
 }
 
 export enum PageSizes {
   A4 = 'A4',
   LETTER = 'LETTER',
 }
+export const defaultStyle = {
+  font: Fonts.Inter_400,
+  fontSize: 10,
+};
+
+
+/*Ejemplo de estilo sugerido*/
+
+export const headerStyle400 = {
+  fontSize: 10,
+  color: Colors.WHITE,
+};
+
+export const headerStyle500 = {
+  ...headerStyle400,
+  font: Fonts.Inter_500,
+};
+
+export const regularStyle = {
+  ...headerStyle400,
+  font: Fonts.Inter_400,
+};
