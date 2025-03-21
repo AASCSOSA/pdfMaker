1b94f4470e9d6e5d7bec2d82ba18f1a79aa4513f
Alain Sosa <sosacruzalainantonio@gmail.com>
2025-03-21 17:12:50 -0600

    final

--- src/components/order-form/table-products/products-table-style.ts
+++ src/components/order-form/table-products/products-table-style.ts
@@ -1,40 +1,47 @@
-import { TableCellComponent } from "../../tables/table-cell.component";
-import { Colors } from "../../../styles/colors";
-import { Layouts } from "../../../styles/styles";
-import { Fonts } from "../../../styles/fonts";
+import { TableCellComponent } from '../../tables/table-cell.component';
+import { Colors } from '../../../styles/colors';
+import { Layouts } from '../../../styles/styles';
+import { Fonts } from '../../../styles/fonts';
+import { textOverflow } from '../../../helpers/table-text-overflow.helper';
 
 const headerStyle = {
   font: Fonts.RobotoBold,
   fontSize: 10,
-}
+};
 export const headerDescription = new TableCellComponent('Descripción', {
-  ...headerStyle
-}).setFillColor(Colors.Foam).setAlignment(Layouts.Left).setMargin([10, 10, 0, 0])
+  ...headerStyle,
+})
+  .setFillColor(Colors.Foam)
+  .setAlignment(Layouts.Left)
+  .setMargin([10, 10, 0, 0])
   .setActivateBorder(false, false, false, false);
 
 export const headerPrice = new TableCellComponent('Precio Unitario', {
-  ...headerStyle
-}).setActivateBorder(false, false, false, false)
+  ...headerStyle,
+})
+  .setActivateBorder(false, false, false, false)
   .setFillColor(Colors.Foam)
   .setAlignment(Layouts.Left)
   .setMargin([0, 10, 0, 0]);
 
 export const headerQuantity = new TableCellComponent('Cant.', {
-  ...headerStyle
-}).setActivateBorder(false, false, false, true)
+  ...headerStyle,
+})
+  .setActivateBorder(false, false, false, true)
   .setFillColor(Colors.Foam)
   .setAlignment(Layouts.Left)
   .setMargin([0, 10, 0, 0]);
 
 export const headerSubtotal = new TableCellComponent('Subtotal', {
-  ...headerStyle
-}).setActivateBorder(false, false, false, false)
+  ...headerStyle,
+})
+  .setActivateBorder(false, false, false, false)
   .setFillColor(Colors.Foam)
   .setAlignment(Layouts.Left)
   .setMargin([0, 10, 0, 0]);
 
 export const headerIva = new TableCellComponent('IVA', {
-  ...headerStyle
+  ...headerStyle,
 })
   .setActivateBorder(false, false, false, false)
   .setFillColor(Colors.Foam)
@@ -42,7 +49,7 @@ export const headerIva = new TableCellComponent('IVA', {
   .setMargin([0, 10, 0, 0]);
 
 export const headerTotal = new TableCellComponent('Total', {
-  ...headerStyle
+  ...headerStyle,
 })
   .setActivateBorder(false, false, false, false)
   .setFillColor(Colors.Foam)
@@ -51,10 +58,11 @@ export const headerTotal = new TableCellComponent('Total', {
 
 // Product rows
 export const productDescription = (description: string) =>
-  new TableCellComponent(description, {
+  new TableCellComponent(textOverflow(description), {
     font: Fonts.InterRegular,
     fontSize: 10,
-  }).setAlignment(Layouts.Left)
+  })
+    .setAlignment(Layouts.Left)
     .setMargin([10, 14, 30, 0]);
 
 export const productUnitPrice = (unitPrice: number) =>
@@ -67,26 +75,30 @@ export const productQuantity = (quantity: number) =>
   new TableCellComponent('2', {
     font: Fonts.InterRegular,
     fontSize: 10,
-  }).setMargin([0, 14, 0, 0])
+  })
+    .setMargin([0, 14, 0, 0])
     .setAlignment(Layouts.Left);
 
 export const productSubtotal = (subtotal: number) =>
   new TableCellComponent('$240.00', {
     font: Fonts.InterRegular,
     fontSize: 10,
-  }).setMargin([0, 14, 0, 0])
+  })
+    .setMargin([0, 14, 0, 0])
     .setAlignment(Layouts.Left);
 
 export const productIva = (iva: number) =>
   new TableCellComponent('$38.40', {
     font: Fonts.InterRegular,
     fontSize: 10,
-  }).setMargin([0, 14, 0, 0])
+  })
+    .setMargin([0, 14, 0, 0])
     .setAlignment(Layouts.Left);
 
 export const productTotal = (total: number) =>
   new TableCellComponent('$278.40', {
     font: Fonts.InterRegular,
     fontSize: 10,
-  }).setMargin([0, 14, 0, 0])
+  })
+    .setMargin([0, 14, 0, 0])
     .setAlignment(Layouts.Left);
